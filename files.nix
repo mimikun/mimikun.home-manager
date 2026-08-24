e6b953d59bf9a50a9112119b11c33d8d62a15d0f
Yuto Tanaka <git@mimikun.dev>
2026-08-24 15:34:17 +0900

    feat(pi): add /exit command alias

--- files.nix
+++ files.nix
@@ -3,5 +3,18 @@
   home.file = {
     # Add dotfile symlink definitions here
     # ".screenrc".source = dotfiles/screenrc;
+
+    ".pi/agent/extensions/exit-command.ts".text = ''
+      import type { ExtensionAPI } from "@earendil-works/pi-coding-agent";
+
+      export default function (pi: ExtensionAPI) {
+        pi.registerCommand("exit", {
+          description: "Quit pi",
+          handler: async (_args, ctx) => {
+            ctx.shutdown();
+          },
+        });
+      }
+    '';
   };
 }
